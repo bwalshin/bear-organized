748666798decb1ecf72fe4611b6e0a9add03e3d1
Brandon Walshin <brandon.walshin@gmail.com>
2023-01-31 15:05:39 -0800

    Update Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
-    <section className='alert alert-dismissible alert-success'>
-      <p>
-        Logged in!
-      </p>
-    </section>
+    <div className='alert alert-dismissible alert-success'>
+      <p>Logged in!</p>
+    </div>
   );
 }
